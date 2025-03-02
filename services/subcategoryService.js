2b344a26a446b13fc22c9d833a5702225748760b
Hassan <ahmed.hassan@dkilo.com>
2025-03-02 15:32:11 +0200

    Feat: SubCategory - getSubCategories added including (getSubCategoriesValidator-handlers-)

--- services/subcategoryService.js
+++ services/subcategoryService.js
@@ -21,3 +21,30 @@ exports.createSubCategory = asyncHandler(async (req, res, next) => {
     
     res.status(201).json({  success: true, data: subCategory});
     });
+
+// @desc    Get all subcategories
+// @route   GET /api/v1/subcategories
+// @access  Public
+exports.getSubCategories = asyncHandler(async (req, res, next) => {
+     page = parseInt(req.query.page) || 1
+  limit = parseInt(req.query.limit) || 3
+  skip = (page - 1) * limit
+
+    const subcategories = await subCategoryModel.find().skip(skip).limit(limit);
+    res.status(200).json({results : subcategories.length,page : page, data:subcategories});
+  });
+
+
+// @desc    Get a subcategory
+// @route   GET /api/v1/subcategories/:id
+// @access  Public
+exports.getSubCategory = asyncHandler(async (req, res, next) => {
+    const { id } = req.params;
+    const subCategory = await subCategoryModel.findById(id);
+
+    if (!subCategory) {
+        return next(new ApiError(`SubCategory not found with id of ${id}`, 404));
+    }
+    res.status(200).json({ data: subCategory });
+});
+
